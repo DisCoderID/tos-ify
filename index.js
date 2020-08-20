2c0c93b59ba6d155d88f018e03bf6693858c000c
hansputera <47862061+hansputera@users.noreply.github.com>
2020-08-20 22:04:18 +0700

    regex added for validation the text.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const regexAlphabet = new RegExp(/[a-z]/gi);
+
 function add(input) {
+ if (!regexAlphabet.exec(input)) throw Error("Invalid text.");
  return input.split(" ").join("tos ") + "tos";
 };
 
